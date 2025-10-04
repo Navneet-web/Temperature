f5e7ed26bce15d36994a65744b014ec340d88806
Navneet <navneet956044@gmail.com>
2025-10-04 06:26:50 +0530

    Add files via upload
    Here i use to make some funny thing in python (Agar aap temperature Daloge toh Apko ek funny comment karega)....

---
+++ Inequality.py
@@ -0,0 +1,11 @@
+a=float(input("Enter The Temperature (in celcius):: "))
+if -20<=a<=0:
+    print("Very Colddd !!!!")
+elif 1<=a<=20:
+    print("Thodi Si Thand Toh Hai !!!")
+elif 21<=a<=40:
+    print("Ye Toh Normal Hai !!!")
+elif 41<=a<=60:
+    print("Thodi Jyada Garmi Hai !!!")
+else:
+    print("You Will Die In this Garmi !!!")
